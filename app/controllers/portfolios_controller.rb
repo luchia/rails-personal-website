bfd22c51a8199000d20c1a5f86aae4b829e0ac2b
Lucy Bloomfield <hello@luchia.com.au>
2014-01-16 11:42:05 +0700

    Fix login page styling and back end view of portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,7 +5,7 @@ class PortfoliosController < ApplicationController
   # GET /portfolios
   # GET /portfolios.json
   def index
-    @portfolios = Portfolio.all
+    @portfolios = Portfolio.order('created_at DESC')
     render :layout => 'adminlayout'
   end
 
