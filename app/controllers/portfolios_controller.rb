8974083c61ff381d54146858bc6df745a02b586e
Lucy Bloomfield <hello@luchia.com.au>
2014-01-16 12:02:23 +0700

    Add image form, render image on show page and home page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -74,6 +74,6 @@ class PortfoliosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_params
-      params.require(:portfolio).permit(:title, :date, :content)
+      params.require(:portfolio).permit(:title, :date, :content, :image)
     end
 end
