37dd419b9ae34e2bdbf1ed6a9a0064933dfebd11
Lucy Bloomfield <hello@luchia.com.au>
2014-04-04 17:48:29 +1000

    Add validation to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
 	has_many :portfolios, :order => "created_at DESC"
+	validates :tag, presence: true
 	validates :tag, uniqueness: true
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,5 +1,18 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
+	fixtures :tags
+
+	test "tag attributes must not be empty" do
+		tag = Tag.new
+		assert tag.invalid?
+		assert tag.errors[:tag].any?	
+	end
+
+	test "tag is not valid without a unqiue title" do
+		tag = Tag.new(tag: tags(:ruby).tag)
+		assert tag.invalid?
+		assert_equal ["has already been taken"], tag.errors[:tag]
+	end
 
 end
