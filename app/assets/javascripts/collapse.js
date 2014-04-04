2d0d4e990d789c1fbdcda34ce261910a21c18942
Lucy Bloomfield <hello@luchia.com.au>
2014-04-04 17:19:14 +1000

    Attempt to fix mobile navigation

---
+++ app/assets/javascripts/collapse.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('.navbar-toggle').click(function(){
+    	//get collapse content selector
+		var collapse_content_selector = $(this).attr('href');
+
+		console.log("hello world!")
+	});
+});
