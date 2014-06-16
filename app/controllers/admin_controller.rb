101c07a48e893b13ec3214b9f6e8c66e863e27b1
Lucy Bloomfield <hello@luchia.com.au>
2014-06-16 12:35:26 +0100

    Fix more things

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 class AdminController < ApplicationController
 
   def index
+  	@portfolios = Portfolio.order("created_at DESC").limit(1)
+  	@journals = Journal.order("created_at DESC").limit(1)
   end
 
 end
