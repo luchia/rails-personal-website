191cb720473ffb8caba20f5d690f45a478b1af5d
Lucy Bloomfield <hello@luchia.com.au>
2014-07-11 15:32:51 +0200

    new api key for mailchimp

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-  Gibbon::API.api_key = "61eabe152622e1b6b5ad0733b5010a78-us8"
+  Gibbon::API.api_key = "3cc75c4e4268662abc3634fc3a843dd2-us8"
   Gibbon::API.timeout = 15
   Gibbon::API.throws_exceptions = false
